768b358d857c7a59cb7ad51ab3a3ec32c226e567
adamantdude <adam.plee23@gmail.com>
2022-11-07 09:43:44 -0600

    complete detail route and MovieDetail component to display movie details

--- src/components/MovieDetail/MovieDetail.jsx
+++ src/components/MovieDetail/MovieDetail.jsx
@@ -1,11 +1,14 @@
 import './MovieDetail.css';
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom/cjs/react-router-dom";
 
 function MovieDetail() {
     const dispatch = useDispatch();
     const { id } = useParams();
+    const movie = useSelector(store => store.movies)
+
+    console.log(movie);
 
     useEffect(() => {
         dispatch({ type:'FETCH_DETAIL', payload: id })
@@ -13,7 +16,10 @@ function MovieDetail() {
 
     return (
         <div id="movieDetail">
-
+            <img src={movie[0].poster} /> 
+            {movie[0].title}
+            <br></br>
+            {movie[0].description}
         </div>
     )
 }
